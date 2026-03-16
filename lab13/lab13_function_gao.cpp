3516af173237f02983caba144f9eb2ccaa9973fa
Chrisdatalab <chrislife@secoryx.com>
2026-03-16 15:57:51 +0000

    intr function

---
+++ lab13/lab13_function_gao.cpp
@@ -0,0 +1,60 @@
+/*
+    weiting gao
+    lab13 introduce to function
+    Mar 16 ,2026
+*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+// example 1: void function void functions are functions that don't return any value
+void printHello(){
+    cout<<"Hello World!"<<endl;
+    return;
+    // the return line is oprional for a void function
+}
+//  example 2: function that pass values
+void greeting(string username){
+    cout<<"Welcome to function: "<<username<<endl;
+}
+// example 3: function that pass and return values
+// function that passes a number return the triple of th number
+int tripleNumber(int number){
+    return number*3;
+}
+// example 4; function to caculate to check number positive, negative or 0
+// return string
+string checkNumber(int number){
+    if(number>0)
+        return "Positive";
+    else if (number<0)
+    {
+        return "Negative";
+    }
+    else
+        return "Zero";
+}
+// example 5: composition function
+// function validate that a number is positive
+//function only returns a positive number 
+//function 1
+int validatepositive(){
+    int n;
+    cout<<"Enter anumber: ";
+    cin>>n;
+    // recollect the number if the number is not positive
+    while (n<=0)
+    {
+        cout<<"Enter a positive number: "<<endl;
+        cin>>n;
+    }
+    return n;
+}
+// function 2: function to calculate and return the area if a rectangle
+int areaRectangle(int length,int width){
+    return length*width;
+}
+//function3
+void printarea(int length,int width,int area){
+    cout<<"The area of a rectangle with "<<length<< " and "<<width<< " is "<<area<<endl;
+}
---
+++ lab13/lab_gao.cpp
@@ -0,0 +1,32 @@
+/*
+    weiting gao
+    lab13 introduce to function
+    Mar 16 ,2026
+*/
+#include<iostream>
+#include"lab13_function_gao.cpp"
+using namespace std;
+int main(){
+
+    cout<<"\n----example 1: void function-----"<<endl;
+    printHello();
+    printHello();
+    cout<<"\n----example 2: void function pass values-----"<<endl;
+    greeting("John");
+    greeting("bob any");
+    cout<<"\n----example 3: triple a number-----"<<endl;
+    int n = tripleNumber(4);
+    cout<<n<<endl;
+    cout<<tripleNumber(20)<<endl;
+    cout<<"\n----example 4: return string-----"<<endl;
+    cout<<checkNumber(24)<<endl;
+    cout<<checkNumber(0)<<endl;
+    cout<<checkNumber(-452)<<endl;
+    cout<<"\n----example 5: positive composition-----"<<endl;
+    int length=validatepositive();
+    int width=validatepositive();
+    int area_rec=areaRectangle(length,width);
+    cout<<area_rec<<endl;
+    printarea(length,width,area_rec);
+    return 0;
+}
