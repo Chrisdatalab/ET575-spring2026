4dd9996449153bbaac82a39232207db2e5c442da
Weiting <chrislife@secoryx.com>
2026-04-27 15:38:36 +0000

    lab20'

---
+++ lab20/lab20_function_gao.cpp
@@ -0,0 +1,83 @@
+/*
+    weiting gao
+    lab20 , parsing and file handling
+    April 27, 2026
+*/
+#include<iostream>
+using namespace std;
+
+// example 1: using cin to validate a data type
+// if the input is a preper data type, cin will set a state to goodbit
+// if the input is not a preper data type, cin will set a state to failbit
+
+// function to collect and return an integer
+int collection(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(cin.fail()){
+        // clear the failbit 
+        cout<<"Wrong data type!"<<endl;
+        cin.clear();
+        cin.ignore(10000,'\n');
+        //recollect the number
+        return collection();
+    }
+    else{
+        return n;
+    }
+}
+// example 2: read file
+// input file = ifstream
+// output file = ofstream
+// fstream has ifstream and ofstream
+// after the library, we can declare an ifstream and ofstream objects
+#include<fstream>
+void readfile(string filename){
+    // declare an object to handle: ifstream
+    ifstream fin;
+    // declare a variable to hold each line of the read file
+    string line;
+    // declare a variable as a line counter
+    int linecounter = 1; //first line
+    // open the file
+    fin.open(filename);
+    //use a loop to go through each line in thr opened file
+    while (getline(fin,line))
+    {
+        cout<<"Sentence "<<linecounter<<" = "<<line<<endl;
+        linecounter++;
+    }
+    // close the file input while finish reading
+    fin.close();
+}
+
+// example 3: write files
+// function to write or create a new file
+void writefile(string filename){
+    // declare an object to handle output files, fout
+    ofstream fout;
+    // open the file
+    // if the file exists, it will overwrite
+    // if the file doesn't exists,it will create a new file
+    fout.open(filename);
+    // write "student's full name" in the first line of file
+    fout<<"Student's full name\n";
+    fout<<"Lab20 \n";
+    // close the file output when finished writing
+    fout.close();
+}
+
+// example 4; append data into a existing file
+void appendfile(string filename, string ){
+    ofstream fout;
+    fout.open(filename,ios:: app);
+    // append data to the file
+    for (int i = 3; i>=0; i--)
+    {
+        fout<<i<<endl;
+    }
+    fout<<"Game Over!\n"<<endl;
+    fout.close();   
+
+}
---
+++ lab20/lab20_main_gao.cpp
@@ -0,0 +1,26 @@
+/*
+    weiting gao
+    lab20 , parsing and file handling
+    April 27, 2026
+*/
+#include<iostream>
+#include"lab20_function_gao.cpp"
+
+using namespace std;
+int main(){
+    cout<<"--------example 1: using cin to validate a data type---------\n"<<endl;
+    int n=collection();
+    cout<<"Collected number = "<<n<<endl;
+    cout<<"--------example 2: read file---------\n"<<endl;
+    /*string filename;
+    cin>>filename;*/
+    readfile("samplefile.txt");
+
+    cout<<"--------example 3: write files---------\n"<<endl;
+    writefile("name.txt");
+
+    cout<<"--------example 4: appand files---------\n"<<endl;
+    appendfile("samplefile.txt", "student's name" );
+
+    return 0;
+}
